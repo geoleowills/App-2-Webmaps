eca4123d6a7fc9cc8df45540f476a36bc1ce2602
George Willens <willensgeorge@gmail.com>
2020-05-05 18:47:20 +0100

    First draft

--- map1.py
+++ map1.py
@@ -1,22 +1,42 @@
-### tiles = "Mapbox Bright"
+# tiles = "Mapbox Bright"
 # instead use
-### tiles = "Stamen Terrain"
+# tiles = "Stamen Terrain"
 
 import folium
 import pandas
 
+
+def colordecider(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
+
 data = pandas.read_csv("volcano.txt")
 
+latlist = list(data["LAT"])
+lonlist = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[38.58, -99.09],
                  zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
-for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
-    fg.add_child(folium.Marker(location=coordinates,
-                               popup="Hi I am a marker", icon=folium.Icon(color="green")))
+for lt, ln, el in zip(latlist, lonlist, elev):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], popup=str(
+        el)+"m", radius=10, fill_color=colordecider(el), color="grey", fill_opacity=0.7))
 
-map.add_child(fg)
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+                             style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
---
+++ test.py
@@ -0,0 +1,3 @@
+dat = open("world.json", "r")
+dat = dat.read()
+print(dat["properties"]["POP2005"])
